70fe22d3f28cb4dd62d513f68af45150e0b8b541
Tinkoprof <88162886+Tinkoprof@users.noreply.github.com>
2024-12-28 00:43:01 +0300

    patch

--- src/main/java/net/tinkoproff/ported/RPC.java
+++ src/main/java/net/tinkoproff/ported/RPC.java
@@ -1,20 +1,18 @@
 package net.tinkoproff.ported;
 
-import net.arikia.dev.drpc.DiscordEventHandlers;
-import net.arikia.dev.drpc.DiscordRPC;
-import net.arikia.dev.drpc.DiscordRichPresence;
+import club.minnced.discord.rpc.DiscordEventHandlers;
+import club.minnced.discord.rpc.DiscordRPC;
+import club.minnced.discord.rpc.DiscordRichPresence;
 
 public class RPC {
     private static final DiscordRichPresence discordRichPresence = new DiscordRichPresence();
+    private static final DiscordRPC lib = DiscordRPC.INSTANCE;
 
     public static void startRPC() {
-        DiscordEventHandlers eventHandlers = new DiscordEventHandlers.Builder()
-            .setReadyEventHandler((user) -> System.out.println("Discord RPC is ready"))
-            .setDisconnectedEventHandler((errorCode, message) -> System.out.println("Discord RPC disconnected, error: " + errorCode + ", message: " + message))
-            .build();
-
-        String discordID = "838078740344471623";
-        DiscordRPC.discordInitialize(discordID, eventHandlers, true);
+        DiscordEventHandlers handlers = new DiscordEventHandlers();
+        handlers.ready = (user) -> System.out.println("Discord RPC is ready");
+        handlers.disconnected = (errorCode, message) -> System.out.println("Discord RPC disconnected, error: " + errorCode + ", message: " + message);
+        lib.Discord_Initialize("838078740344471623", handlers, true, null);
 
         discordRichPresence.startTimestamp = System.currentTimeMillis() / 1000L;
         discordRichPresence.details = "WP3 - Ported by Tinkoprof (respect to travisF)";
@@ -22,10 +20,10 @@ public class RPC {
         discordRichPresence.largeImageText = "Ported by Tinkoprof";
         discordRichPresence.smallImageKey = "small";
         discordRichPresence.smallImageText = "discord.gg/klux";
-        DiscordRPC.discordUpdatePresence(discordRichPresence);
+        lib.Discord_UpdatePresence(discordRichPresence);
     }
 
     public static void stopRPC() {
-        DiscordRPC.discordShutdown();
+        lib.Discord_Shutdown();
     }
 }
